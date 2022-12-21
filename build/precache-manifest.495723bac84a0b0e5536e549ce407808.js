34c67eeb1e1c610f043565bb5b89ed4358fcd147
Jose Antonio Córdoba Gómez <pp@Joses-MacBook-Pro.local>
2022-12-21 11:51:30 +0100

    client

---
+++ build/precache-manifest.495723bac84a0b0e5536e549ce407808.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374a209b7e68e43aaf2264ade068a2dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "160cf538ad787aa8d13f",
+    "url": "/static/css/2.85ea3186.chunk.css"
+  },
+  {
+    "revision": "403d170bd04037470efa",
+    "url": "/static/css/main.49ade188.chunk.css"
+  },
+  {
+    "revision": "160cf538ad787aa8d13f",
+    "url": "/static/js/2.2e0361d1.chunk.js"
+  },
+  {
+    "revision": "6d5748d8907d6ae93a0a88b16360b2c2",
+    "url": "/static/js/2.2e0361d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403d170bd04037470efa",
+    "url": "/static/js/main.27c138d9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.27c138d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "ee6aadae18d9da389dc08dd8d72e3721",
+    "url": "/static/media/correct-answer.ee6aadae.mp3"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/static/media/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/static/media/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/static/media/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/static/media/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "c7106542fc4e8075bf1bae4bf6854692",
+    "url": "/static/media/wrong-answer.c7106542.mp3"
+  }
+]);
